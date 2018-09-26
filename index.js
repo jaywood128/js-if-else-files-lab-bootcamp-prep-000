b2e65e482df4deb20b4a6e8117d06a657e5a00cd
Johnathon Wood <jaywood128@gmail.com>
2018-09-26 14:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var name = "Joe"
 var height = "74"
+if (name === "Susan") {
+  console.log("The name is correct")
+  else {
+    `Expected name: Susan, Received: ${name}` 
+  }
+}
+if (height === 70) {
+  console.log "The height is correct"
+} else{
+  `Expected: 70, Received:${height} `
+  }
 
 
 // Don't worry about this
